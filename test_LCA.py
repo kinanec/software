cc762f12cb2c58a647668a87b529d1f9615d53cf
kinanec <32737579+kinanec@users.noreply.github.com>
2018-09-27 13:04:48 +0100

    test for when both nodes are the root

--- test_LCA.py
+++ test_LCA.py
@@ -6,3 +6,7 @@ import LCA
 #test result for no input
 def test_answer():
     assert LCA.findLCA(None, None, None) == None
+
+def test_answer1():
+    root = LCA.Node(1)
+    assert LCA.findLCA(root,1,1) == 1
