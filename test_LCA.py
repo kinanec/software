08f8848fbee0fbabd4f1c2393bcd5ef122ea2612
kinanec <32737579+kinanec@users.noreply.github.com>
2018-09-27 13:32:55 +0100

    test for n1 = root and n2 != root and for a tree with depth greater than one

--- test_LCA.py
+++ test_LCA.py
@@ -21,3 +21,14 @@ def test_answer2():
 #test for n1 is root.left and root.right
     root.right = LCA.Node(10)
     assert LCA.findLCA(root, 4,10) == 6
+
+#test to make recursion happen
+    root.left.left = LCA.Node(2)
+    root.left.right = LCA.Node(5)
+    root.right.left = LCA.Node(7)
+    root.right.right = LCA.Node(11)
+    assert LCA.findLCA(root, 2,11) == 6
+    assert LCA.findLCA(root, 7,11) == 10
+
+# test where one n1 is root and n2 is another tree element
+    assert LCA.findLCA(root, 6,10) == 6
