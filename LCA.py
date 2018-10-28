edb8428bced6d7eed31509050b100b7dca4f5420
kinanec <32737579+kinanec@users.noreply.github.com>
2018-10-28 12:13:17 +0000

    add find_all_paths def from first iteration of the assignement

--- LCA.py
+++ LCA.py
@@ -14,3 +14,16 @@ def findLCA(graph, root, a, b):
         print(str(b) + " does not exist in the graph.")
         return None
     
+def find_all_paths(graph, start, end, path=[]):
+    path = path + [start]
+    if start == end:
+        return [path]
+    if not graph.has_node(start):
+        return []
+    paths = []
+    for node in graph[start]:
+        if node not in path:
+            newpaths = find_all_paths(graph, node, end, path)
+            for newpath in newpaths:
+                paths.append(newpath)
+    return paths
