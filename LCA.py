a7fc94e3e3c8d4d0470b233b20c9b1e07b9aef07
kinanec <32737579+kinanec@users.noreply.github.com>
2018-10-28 12:14:59 +0000

    use the find_all_paths to retrieve list of paths

--- LCA.py
+++ LCA.py
@@ -1,5 +1,7 @@
 import networkx as nx
 G = nx.DiGraph()
+G.add_nodes_from([1, 10])
+G.add_edges_from([(1,2), (1,3), (1,5), (2,4), (2,5), (3,6), (3,7), (4,8), (5,9), (6,10)])
 
 def findLCA(graph, root, a, b):
 
@@ -13,7 +15,13 @@ def findLCA(graph, root, a, b):
     if(not graph.has_node(b)):
         print(str(b) + " does not exist in the graph.")
         return None
+    trace = []
+    pathsA = find_all_paths(G,root,a)
+    pathsB = find_all_paths(G,root,b)
+    tracer.append(pathsA)
+    tracer.append(pathsB)
     
+
 def find_all_paths(graph, start, end, path=[]):
     path = path + [start]
     if start == end:
