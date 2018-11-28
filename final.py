7639f74f09f8df84644600df3aafbac29f55efd9
kinanec <32737579+kinanec@users.noreply.github.com>
2018-11-28 19:07:58 +0000

    gets language information on the most starred repo

--- final.py
+++ final.py
@@ -62,3 +62,111 @@ def topRepo():
     return maxRepo
 
 topRepo()
+
+def languages():
+    repoContent = []
+    languageList = []
+    #Map for file extentions to programing language
+    languageDict = {
+        'CS' : 'C#',
+        'HPP':	'C++',
+        'CLASS' : 'Java',
+        'CPP' : 'C++',
+        'ERB' : 'Ruby',
+        'CP' : 'C++',
+        'MF' : 'Java',
+        'DMD' : 'SQL',
+        'JAVA' : 'Java',
+        'PY' : 'Python',
+        'RES' : 'C++',
+        'SWIFT' : 'Swift',
+        'XSD' : 'XML',
+        'RBW' : 'Ruby',
+        'CLW' : 'C++',
+        'NCB' : 'C++',
+        'SH' : 'Shell',
+        'RPY' : 'Python',
+        'HH' :	'C++',
+        'CC' : 'C++',
+        'PYD' : 'Python',
+        'R' : 'R',
+        'APS' : 'C++',
+        'HS' : 'Haskell',
+        'JS' : 'JavaScript',
+        'HTML' : 'HTML',
+        'CSS' : 'CSS',
+        'PHP' : 'PHP'
+    }
+    for key, val in languageDict.items():
+        print (key)
+        languageList.append(key)
+    x = getFollowerStars()
+    mostStarred = x[0][0]
+    repo = g.get_user(mostStarred).get_repo(topRepo())
+    #print(repo)
+    repoDetails = { 'Name' : repo.name,
+                        'Total' : 0         }
+    contents = repo.get_contents('')
+    print(contents)
+    for content_file in contents:
+        if content_file.type == 'dir':
+            contents.extend(repo.get_contents(content_file.path))
+
+    langList = []
+    sizeList = []
+    for content_file in contents:
+        if "." in content_file.path:
+            split = content_file.path.split('.')
+            if split[1].upper() in languageDict:
+                if split[1].upper()  in langList:
+                    index = langList.index(split[1].upper())
+                    newTotal = sizeList[index] + content_file.size
+                    sizeList[index] = newTotal
+                else:
+                    langList.append(split[1].upper())
+                    sizeList.append(content_file.size)
+    print(langList)
+    print(sizeList)
+    #print("this is a list of langs and size of files", langList)
+languages()
+#if split[1].upper() in langList:
+#    langList[langList.index(split[1])]
+#langList.append([split[1],content_file.size])
+#        print("this is file size", content_file.size)
+#        repoContent.append(content_file.path)
+#        print(content_file.path)
+#    print(contents)
+
+
+
+
+#contents = repo.get_contents('')
+##print(contents)
+#for content_file in contents:
+#    if content_file.type == 'dir':
+#        contents.extend(repo.get_contents(content_file.path))
+#    for repo in content_file.name:
+#        if '.' in repo:
+#            split = repo.split('.')
+#            if split[1].upper in languageDict:
+#                extension_size.update({split[1]: content_file.size})
+#                #if split[1] not in extension_size:
+#                #    extension_size.update({split[1]: content_file.size})
+                #else:
+                #    extension_size.setdefault(split[1], )[split[1] += content_file.size]
+#    print(extension_size)
+
+
+#        filesizes.append(content_file.size)
+#        repoContent.append(content_file.name)
+#        #print(content_file.path)
+##    print(repoContent)
+#    print(filesizes)
+#    for repo in repoContent:
+##        if '.'in repo:
+#            split = repo.split('.')
+#            extensions.append(split[1])
+#    print (extensions)
+
+
+languages()
