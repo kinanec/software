44fabd800ef509a6e282deda6dbe3ccf982a51ca
kinanec <32737579+kinanec@users.noreply.github.com>
2018-11-28 21:01:03 +0000

    first visualisation- bar chart of followers stars

---
+++ visualisation.py
@@ -0,0 +1,39 @@
+import plotly
+import plotly.plotly as py
+import plotly.graph_objs as go
+import final
+
+#==========================================
+#       Visualisation #1
+#==========================================
+
+users, stars = zip(*final.followerList)
+#print(users)
+#print(stars)
+#data = [go.Bar(
+#            x=users,
+#            y=stars
+#    )]
+
+#py.iplot(data, filename='basic-bar')
+
+x = users
+y = stars
+
+data = [go.Bar(
+            x=x,
+            y=y,
+            text=y,
+            textposition = 'auto',
+            marker=dict(
+                color='rgb(158,202,225)',
+                line=dict(
+                    color='rgb(8,48,107)',
+                    width=1.5),
+            ),
+            opacity=0.6
+        )]
+
+py.plot(data, filename='bar-direct-labels v2')
+
+# https://plot.ly/~kinanec/6/#/
